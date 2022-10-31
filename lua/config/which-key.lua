1f4468d8d511a6baf7f490d70f567e6b623cdb92
Luke Glazebrook <Glazy@users.noreply.github.com>
2022-10-31 19:45:10 +0000

    Make <Leader>g open LazyGit

--- lua/config/which-key.lua
+++ lua/config/which-key.lua
@@ -20,9 +20,7 @@ function M.setup()
   local mappings = {
     e = { ':Telescope file_browser<CR>', 'Toggle Explorer' },
     ['/'] = { '<Plug>--[[ (comment_toggle_linewise_current) ]]', 'Toggle comment' },
-    g = {
-      g = { ':FloatermNew --disposable lazygit<CR>', 'LazyGit' },
-    },
+		g = { ':FloatermNew --disposable lazygit<CR>', 'LazyGit' },
     f = {
       name = 'find',
       f = { '<cmd>Telescope find_files<CR>', 'Find file' },
