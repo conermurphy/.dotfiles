ca01f30cdbff505950995e8530a6cf09193354c6
Luke Glazebrook <Glazy@users.noreply.github.com>
2022-11-01 17:51:20 +0000

    Make <CR> complete snippet with nvim-cmp

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -1,67 +1,67 @@
 local M = {}
 
 function M.setup()
-  local cmp = require('cmp')
-  local ls = require('luasnip')
+	local cmp = require("cmp")
+	local ls = require("luasnip")
 
-  function has_words_before()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-  end
-
-  cmp.setup({
-    snippet = {
-      expand = function(args)
-	ls.lsp_expand(args.body)
-      end,
-    },
-    mapping = cmp.mapping.preset.insert({
-      ['<Tab>'] = cmp.mapping(function(fallback)
-	if cmp.visible() then
-	  cmp.select_next_item()
-	elseif ls.expand_or_jumpable() then
-	  ls.expand_or_jump()
-	elseif has_words_before() then
-	  cmp.complete()
-	else
-	  fallback()
+	local has_words_before = function()
+		local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+		return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 	end
-      end, { 'i', 's' }),
-      ['<S-Tab>'] = cmp.mapping(function(fallback)
-	if cmp.visible() then
-	  cmp.select_prev_item()
-	elseif ls.jumpable(-1) then
-	  ls.jump(-1)
-	else
-	  fallback()
-	end
-      end, { 'i', 's' }),
-      ['<C-Space>'] = cmp.mapping.complete(),
-    }),
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'luasnip' },
-    }, {
-      { name = 'buffer' },
-    }),
-  })
 
-  cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = 'buffer' },
-    },
-  })
+	cmp.setup({
+		snippet = {
+			expand = function(args)
+				ls.lsp_expand(args.body)
+			end,
+		},
+		mapping = cmp.mapping.preset.insert({
+			["<Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_next_item()
+				elseif ls.expand_or_jumpable() then
+					ls.expand_or_jump()
+				elseif has_words_before() then
+					cmp.complete()
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
+			["<S-Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_prev_item()
+				elseif ls.jumpable(-1) then
+					ls.jump(-1)
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
+			["<C-Space>"] = cmp.mapping.complete(),
+			["<CR>"] = cmp.mapping.confirm({ select = true }),
+		}),
+		sources = cmp.config.sources({
+			{ name = "nvim_lsp" },
+			{ name = "luasnip" },
+		}, {
+			{ name = "buffer" },
+		}),
+	})
 
-  cmp.setup.cmdline({ ':' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = 'path' },
-    }, {
-      { name = 'cmdline' },
-    })
-  })
+	cmp.setup.cmdline({ "/", "?" }, {
+		mapping = cmp.mapping.preset.cmdline(),
+		sources = {
+			{ name = "buffer" },
+		},
+	})
+
+	cmp.setup.cmdline({ ":" }, {
+		mapping = cmp.mapping.preset.cmdline(),
+		sources = cmp.config.sources({
+			{ name = "path" },
+		}, {
+			{ name = "cmdline" },
+		}),
+	})
 end
 
 return M
-
--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,16 +1,16 @@
-require('mason').setup()
-require('mason-lspconfig').setup()
+require("mason").setup()
+require("mason-lspconfig").setup()
 
 local M = {}
 
 function M.setup()
-	local lsp = require('lspconfig')
+	local lsp = require("lspconfig")
 
 	function on_attach(client, bufnr)
-		vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+		vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-		vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
-			border = 'single',
+		vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+			border = "single",
 		})
 
 		function create_opts(description)
@@ -22,30 +22,29 @@ function M.setup()
 			}
 		end
 
-		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, create_opts('Go to definition'))
-		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, create_opts('Go to declaration'))
-		vim.keymap.set('n', 'gh', vim.lsp.buf.hover, create_opts('Hover Definition'))
-		vim.keymap.set('n', 'gr', vim.lsp.buf.references, create_opts('References'))
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, create_opts("Go to definition"))
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, create_opts("Go to declaration"))
+		vim.keymap.set("n", "gh", vim.lsp.buf.hover, create_opts("Hover Definition"))
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, create_opts("References"))
 	end
 
-	local capabilities = require('cmp_nvim_lsp').default_capabilities()
+	local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-	lsp.tsserver.setup {
+	lsp.tsserver.setup({
 		on_attach = on_attach,
 		capabilities = capabilities,
-	}
+	})
 
-	lsp.elixirls.setup {
-		cmd = { 'elixir-ls' },
+	lsp.elixirls.setup({
+		cmd = { "elixir-ls" },
 		on_attach = on_attach,
 		capabilities = capabilities,
-	}
+	})
 
-	lsp.sumneko_lua.setup {
+	lsp.sumneko_lua.setup({
 		on_attach = on_attach,
 		capabilities = capabilities,
-	}
+	})
 end
 
 return M
-
