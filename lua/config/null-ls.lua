907a52bfc08a62e0a857933ff0bd97506ff94f34
Luke Glazebrook <Glazy@users.noreply.github.com>
2022-11-01 16:06:18 +0000

    Add Lua formatting via null-ls w/ StyLua

--- lua/config/null-ls.lua
+++ lua/config/null-ls.lua
@@ -1,14 +1,14 @@
 local M = {}
 
 function M.setup()
-	local nls = require('null-ls')
+	local nls = require("null-ls")
 
-	nls.setup {
+	nls.setup({
 		sources = {
+			nls.builtins.formatting.stylua,
 			nls.builtins.formatting.prettierd,
 		},
-	}
+	})
 end
 
 return M
-
