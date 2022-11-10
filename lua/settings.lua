cb628abb26c7dedcb40b060bd0f37ba913a232b7
Luke Glazebrook <Glazy@users.noreply.github.com>
2022-11-10 14:56:00 +0000

    Use spaces instead of tabs

--- lua/settings.lua
+++ lua/settings.lua
@@ -2,25 +2,25 @@ local g = vim.g
 
 vim.cmd([[colorscheme night-owl]])
 
-g.mapleader = ' '
+g.mapleader = " "
 
 local options = {
-  backup = false, -- Creates a backup file
-  ignorecase = true, -- Ignore casing in search patterns
-  splitbelow = true, -- Force all vertical splits to appear beneath current window
-  splitright = true, -- Force all horizontal splits to appear right of current window
-  termguicolors = true,
-  syntax = 'on',
-  shiftwidth = 2,
-  tabstop = 2,
-  cursorline = true,
-  smartindent = true,
-  number = true,
-  relativenumber = true,
-  timeoutlen = 500,
+	backup = false, -- Creates a backup file
+	ignorecase = true, -- Ignore casing in search patterns
+	splitbelow = true, -- Force all vertical splits to appear beneath current window
+	splitright = true, -- Force all horizontal splits to appear right of current window
+	termguicolors = true,
+	syntax = "on",
+	shiftwidth = 2,
+	tabstop = 2,
+	expandtab = true,
+	cursorline = true,
+	smartindent = true,
+	number = true,
+	relativenumber = true,
+	timeoutlen = 500,
 }
 
 for k, v in pairs(options) do
-  vim.opt[k] = v
+	vim.opt[k] = v
 end
-
