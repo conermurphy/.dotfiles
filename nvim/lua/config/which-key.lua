ee3082475da7f5b0b92149bdfddd4e45524fada6
Luke Glazebrook <Glazy@users.noreply.github.com>
2022-11-14 20:55:23 +0000

    Move comment keybinds

--- nvim/lua/config/which-key.lua
+++ nvim/lua/config/which-key.lua
@@ -3,51 +3,33 @@ local M = {}
 function M.setup()
 	local wk = require("which-key")
 
-	local config = {
-		plugins = {
-			marks = false,
-			registers = false,
-			presets = {
-				g = false,
-			},
-		},
-	}
+	local config = {}
 
 	local opts = {
+		mode = "n",
 		prefix = "<leader>",
 	}
 
 	local mappings = {
-		e = { ":Dirbuf<CR>", "Toggle Explorer" },
-		["/"] = { "<Plug>(comment_toggle_linewise_current)", "Toggle comment" },
-		g = { ":FloatermNew --disposable lazygit<CR>", "LazyGit" },
+		e = { "<cmd>Dirbuf<CR>", "Toggle Explorer" },
+		g = { "<cmd>FloatermNew --disposable lazygit<CR>", "LazyGit" },
 		f = {
 			name = "find",
 			f = { "<cmd>Telescope find_files<CR>", "Find file" },
 			g = { "<cmd>Telescope live_grep<CR>", "Find text" },
 			r = { "<cmd>Telescope oldfiles<CR>", "Find recent" },
-			p = { ":Dirbuf vim.fn.getcwd()<CR>", "Find in project" },
+			p = { "<cmd>Dirbuf vim.fn.getcwd()<CR>", "Find in project" },
 		},
 		b = {
 			name = "buffers",
 			f = { "<cmd>Telescope buffers<CR>", "Find" },
 		},
-		p = { ":lua vim.lsp.buf.format({ async = true })<CR>", "Prettify" },
-	}
-
-	local v_opts = {
-		prefix = "<leader>",
-		mode = "v",
-	}
-
-	local v_mappings = {
-		["/"] = { "<Plug>(comment_toggle_linewise_visual)", "Toggle comment" },
+		p = { "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", "Prettify" },
 	}
 
 	wk.setup(config)
 
 	wk.register(mappings, opts)
-	wk.register(v_mappings, v_opts)
 end
 
 return M
--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,8 +1,15 @@
 -- Window navigation and manipulation.
-vim.keymap.set('n', '<C-h>', '<C-w>h')
-vim.keymap.set('n', '<C-j>', '<C-w>j')
-vim.keymap.set('n', '<C-k>', '<C-w>k')
-vim.keymap.set('n', '<C-l>', '<C-w>l')
-vim.keymap.set('n', '<C-i>', '<cmd>:tab split<CR>')
-vim.keymap.set('n', '<Esc>', '<cmd>:nohl<CR>')
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-n>", "<cmd>tab split<CR>")
+vim.keymap.set("n", "<Esc>", "<cmd>nohl<CR>")
 
+-- Commenting
+vim.keymap.set("n", "<C-_>", "<Plug>(comment_toggle_linewise_current)")
+vim.keymap.set("v", "<C-_>", "<Plug>(comment_toggle_linewise_visual)")
+
+-- Tabs
+vim.keymap.set("n", "<Tab>", "<cmd>tabnext<CR>")
+vim.keymap.set("n", "<S-Tab>", "<cmd>tabprevious<CR>")
