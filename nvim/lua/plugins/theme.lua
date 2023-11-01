f93c47a997258eac46fe922a904d17df183a35bd
Luke Glazebrook <Glazy@users.noreply.github.com>
2023-11-01 21:09:10 +0000

    Move to Everforest theme from Catppuccin

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,9 +1,9 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  lazy = false, -- Make sure the theme loads immediately
-  priority = 1000, -- Load before anything else
-  config = function()
-    vim.cmd([[colorscheme catppuccin-mocha]])
-  end,
+	"sainnhe/everforest",
+	lazy = false, -- Make sure the theme loads immediately
+	priority = 1000, -- Load before anything else
+	config = function()
+		vim.g.everforest_background = "medium"
+		vim.cmd([[colorscheme everforest]])
+	end,
 }
