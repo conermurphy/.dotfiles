ff625b38eb621c83202756e272b069a8c334d71c
Luke Glazebrook <Glazy@users.noreply.github.com>
2024-02-28 22:05:25 +0000

    Add mini.pairs Nvim plugin

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -5,6 +5,7 @@ return {
     config = function ()
       require('mini.ai').setup()
       require('mini.surround').setup()
+      require('mini.pairs').setup()
       require('mini.statusline').setup()
     end
   }
