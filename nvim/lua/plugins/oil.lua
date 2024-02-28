7212a645dc9644ed53110013b76b355cee971221
Luke Glazebrook <Glazy@users.noreply.github.com>
2024-02-28 22:18:57 +0000

    Make Oil split keybinds match FzfLua

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,7 +2,12 @@ return {
   'stevearc/oil.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   lazy = false,
-  opts = {},
+  opts = {
+    keymaps = {
+      ['<C-v>'] = 'actions.select_vsplit',
+      ['<C-s>'] = 'actions.select_split',
+    },
+  },
   keys = {
     { '<leader>e', function() require('oil').toggle_float() end, desc = 'Explore' },
     { '<leader>fp', function() require('oil').open(vim.fn.getcwd()) end, desc = 'In Project' },
