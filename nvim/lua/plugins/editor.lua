cb66c120453e2ecdb92dd7c2f609d11134d12bac
Coner Murphy <coner@conermurphy.com>
2024-04-05 20:37:59 +0100

    feat: configured mini move

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -14,6 +14,27 @@ return {
       require('mini.ai').setup()
       require('mini.surround').setup()
       require('mini.pairs').setup()
+      require('mini.move').setup({
+        mappings = {
+          -- Move visual selection in Visual mode.
+          left = '<C-h>',
+          right = '<C-l>',
+          down = '<C-j>',
+          up = '<C-k>',
+
+          -- Move current line in Normal mode
+          line_left = '<C-h>',
+          line_right = '<C-l>',
+          line_down = '<C-j>',
+          line_up = '<C-k>',
+        },
+
+        -- Options which control moving behavior
+        options = {
+          -- Automatically reindent selection during linewise vertical move
+          reindent_linewise = true,
+        },
+      })
       require('mini.statusline').setup()
       require('mini.comment').setup({
         options = {
