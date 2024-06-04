f9b1edfab514a582c96a63d726f472f92bfe28b9
Coner Murphy <coner@conermurphy.com>
2024-06-04 20:38:57 +0100

    chore: update keybinds

--- nvim/lua/plugins/autocomplete.lua
+++ nvim/lua/plugins/autocomplete.lua
@@ -23,8 +23,8 @@ return {
       mapping = cmp.mapping.preset.insert({
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-space>'] = cmp.mapping.confirm({ select = true }),
-        ['<C-s>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-l>'] = cmp.mapping(function()
           if luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
