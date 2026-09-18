f48c924fd1e608360fc9b799bfb3bb9c24732c72
Coner Murphy <conermurphy@users.noreply.github.com>
2026-09-18 17:02:17 +0100

    chore: fix treesitter config

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -67,7 +67,7 @@ return {
     'windwp/nvim-ts-autotag',
     dependencies = 'nvim-treesitter/nvim-treesitter',
     config = function()
-      require('nvim-treesitter.configs').setup({})
+      require('nvim-treesitter').setup({})
     end,
     lazy = true,
     event = 'VeryLazy',
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,26 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
+  lazy = false,
+  branch = 'main',
   config = function()
-    require('nvim-treesitter.configs').setup({
-      ensure_installed = {
-        'html',
-        'lua',
-        'vim',
-        'vimdoc',
-        'typescript',
-        'tsx',
-        'javascript',
-      },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function()
+        -- Enable treesitter highlighting and disable regex syntax
+        pcall(vim.treesitter.start)
+        -- Enable treesitter-based indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+
+    require('nvim-treesitter').install({
+      'html',
+      'lua',
+      'vim',
+      'vimdoc',
+      'typescript',
+      'tsx',
+      'javascript',
     })
     require('nvim-ts-autotag').setup({
       opts = {
